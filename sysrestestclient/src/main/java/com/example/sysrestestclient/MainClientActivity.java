909a0690b9d1481407552c38d700782d06d9115f
TAT-Hins <hins011204@qq.com>
2021-06-27 04:24:40 +0800

    add broadcast send/receive over two apps

--- sysrestestclient/src/main/java/com/example/sysrestestclient/MainClientActivity.java
+++ sysrestestclient/src/main/java/com/example/sysrestestclient/MainClientActivity.java
@@ -2,15 +2,24 @@ package com.example.sysrestestclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class MainClientActivity extends AppCompatActivity {
-
-
+    private static final String TAG = "MainClientActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_client);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent("com.example.sysrestestserver.MainServerActivity.AlarmListener");
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, intent, 0);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 5, pi);
     }
 }
--- sysrestestserver/src/main/java/com/example/sysrestestserver/MainServerActivity.java
+++ sysrestestserver/src/main/java/com/example/sysrestestserver/MainServerActivity.java
@@ -2,8 +2,14 @@ package com.example.sysrestestserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +17,7 @@ import android.widget.TextView;
 import java.lang.invoke.MethodType;
 
 public class MainServerActivity extends AppCompatActivity {
+    private static final String TAG = "MainServerActivity";
 
     private Button mClickButton;
     private TextView mTextView;
@@ -29,12 +36,32 @@ public class MainServerActivity extends AppCompatActivity {
                 addText("Click " + (++clickCount) + " times!");
             }
         });
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(getApplication().getPackageName() + ".MainServerActivity.AlarmListener");
+        registerReceiver(new AlarmListener(), filter);
     }
 
     private void addText(String msg) {
         double curTimeMs = SystemClock.elapsedRealtime() / 1000d;
         if (mTextView != null) {
             mTextView.append("[" + curTimeMs + "ms] " + msg + "\n");
+        }
+    }
+
+    public class AlarmListener extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (context == null || intent == null) {
+                return;
+            }
+            String action = intent.getAction();
+            addText("receive broadcast, action=" + action);
+            if (action.equals(getApplication().getPackageName() + ".MainServerActivity.AlarmListener")) {
+                Log.i(TAG, "receive dynamic broadcast successful!");
+            } else {
+                Log.e(TAG, "receive unexcepted broadcast!");
+            }
         }
     }
 }
